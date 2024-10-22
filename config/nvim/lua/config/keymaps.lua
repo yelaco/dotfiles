0de9c1ea97425a42d9ef51cae2b159396e86d0d7
Quang Bui M. <minhquangbui053@gmail.com>
2024-10-22 22:45:27 +0700

    Update nvim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+local map = vim.keymap.set
+
+map("n", "<leader><S-r>", "", { desc = "Rest"})
+map("n", "<leader><S-r>o", "<cmd>Rest open<cr>", { desc = "Open"})
+map("n", "<leader><S-r>s", "<cmd>Rest run<cr>", { desc = "Send request"})
+map("n", "<leader><S-r>r", "<cmd>Rest last<cr>", { desc = "Send last request"})
+map("n", "<leader><S-r>l", "<cmd>Rest logs<cr>", { desc = "View logs"})
+map("n", "<leader><S-r>es", "<cmd>Rest env show<cr>", { desc = "Show available env"})
+map("n", "<leader><S-r>ej", "<cmd>Rest env select<cr>", { desc = "Select env"})
