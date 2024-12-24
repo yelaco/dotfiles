c89a039668ab625db016d0ec7a4d2908a636865f
Quang Bui M. <minhquangbui053@gmail.com>
2024-12-24 10:54:03 +0700

    Update nvim

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -117,3 +117,26 @@ require('litee.gh').setup({
   }
 })
 vim.cmd("FzfLua register_ui_select")
+
+-- autobackup
+local lazy_cmds = vim.api.nvim_create_augroup('lazy_cmds', {clear = true})
+local snapshot_dir = vim.fn.stdpath('data') .. '/plugin-snapshot'
+local lockfile = vim.fn.stdpath('config') .. '/lazy-lock.json'
+
+vim.api.nvim_create_user_command(
+  'BrowseSnapshots',
+  'edit ' .. snapshot_dir,
+  {}
+)
+
+vim.api.nvim_create_autocmd('User', {
+  group = lazy_cmds,
+  pattern = 'LazyUpdatePre',
+  desc = 'Backup lazy.nvim lockfile',
+  callback = function(event)
+    vim.fn.mkdir(snapshot_dir, 'p')
+    local snapshot = snapshot_dir .. os.date('/%Y-%m-%dT%H:%M:%S.json')
+
+    vim.loop.fs_copyfile(lockfile, snapshot)
+  end,
+})
