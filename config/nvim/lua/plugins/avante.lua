a4a07853c97b7d429bff353378687476c6ac2060
Quang M. Bui <minhquangbui053@gmail.com>
2025-06-06 08:20:11 +0000

    Update neovim config

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -41,6 +41,9 @@ return {
           use_absolute_path = true,
         },
       },
+      keys = {
+        { "<leader>ap", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+      },
     },
     {
       -- Make sure to set this up properly if you have lazy=true
