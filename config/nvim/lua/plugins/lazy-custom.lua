54542152cea4e10ab2d1a732ed7257832535fe19
Quang M. Bui <minhquangbui053@gmail.com>
2025-09-03 17:43:12 +0700

    Update

--- config/nvim/lua/plugins/lazy-custom.lua
+++ config/nvim/lua/plugins/lazy-custom.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
