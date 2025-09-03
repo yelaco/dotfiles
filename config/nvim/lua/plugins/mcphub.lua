03ad22d374f5ffbcfdc50215d3fa349538da79c5
Quang M. Bui <minhquangbui053@gmail.com>
2025-09-03 21:21:43 +0700

    Update

--- config/nvim/lua/plugins/mcphub.lua
+++ config/nvim/lua/plugins/mcphub.lua
@@ -3,10 +3,8 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    build = "bundled_build.lua",  -- Bundles `mcp-hub` binary along with the neovim plugin
     config = function()
         require("mcphub").setup({
-            use_bundled_binary = true,  -- Use local `mcp-hub` binary
             extensions = {
                 avante = {
                     make_slash_commands = true, -- make /slash commands from MCP server prompts
