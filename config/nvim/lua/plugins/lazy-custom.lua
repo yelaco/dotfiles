193e26f33ca8221574d4890ad407eedc228902b6
Quang M. Bui <minhquangbui053@gmail.com>
2025-12-24 16:46:54 +0700

    Update

--- config/nvim/lua/plugins/lazy-custom.lua
+++ config/nvim/lua/plugins/lazy-custom.lua
@@ -83,6 +83,28 @@ return {
       end
     end,
   },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      'Kaiser-Yang/blink-cmp-avante',
+    },
+    opts = {
+      sources = {
+        -- Add 'avante' to the list
+        default = { 'avante', 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          avante = {
+            module = 'blink-cmp-avante',
+            name = 'Avante',
+              opts = {
+                -- options for blink-cmp-avante
+              }
+            }
+          },
+      },
+      fuzzy = { implementation = "lua" },
+    }
+  }
   -- {
   --   "zbirenbaum/copilot.lua",
   --   opts = {
